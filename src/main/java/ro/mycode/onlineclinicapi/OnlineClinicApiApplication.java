ad31e9de205adc3e0591c5aad324abb1b5017038
Balanescu Raul <balanescuraul@gmail.com>
2023-05-21 14:31:50 +0300

    add main

---
+++ src/main/java/ro/mycode/onlineclinicapi/OnlineClinicApiApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.onlineclinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineClinicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/onlineclinicapi/OnlineClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.onlineclinicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
