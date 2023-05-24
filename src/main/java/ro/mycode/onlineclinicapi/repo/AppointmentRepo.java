5cb2b7f5abbc34e41349ca1c0a6c5d4d64c610af
Balanescu Raul <balanescuraul@gmail.com>
2023-05-24 19:45:03 +0300

    add appointment-repo-test

--- src/main/java/ro/mycode/onlineclinicapi/repo/AppointmentRepo.java
+++ src/main/java/ro/mycode/onlineclinicapi/repo/AppointmentRepo.java
@@ -11,23 +11,19 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AppoitmentRepo extends JpaRepository<Appointment,Long> {
+public interface AppointmentRepo extends JpaRepository<Appointment,Long> {
 
 
     @Query("select a from Appointment a where a.number = ?1")
     Optional<Appointment> getAppointmentByNumber(String number);
 
     @Query("select a from Appointment a where a.date = ?1")
-    Optional<Appointment> getAppointmentByDate(LocalDate date);
+    List<Appointment> getAppointmentByDate(LocalDate date);
 
     @Query("select a from Appointment a where a.type = ?1")
     List<Appointment> getAppointmentByType(String type);
 
-    @Query("select a from Appointment a where a.doctor.fullName = ?1")
-    List<Appointment> getAppointmentByDoctorFullName(String fullName);
-
-    @Query("select a from Appointment a where a.doctor.id = ?1")
-    List<Appointment> getAppointmentByDoctorId(long doctorId);
-
+    @Query("select a from Appointment a")
+    List<Appointment> getAllAppointment();
 
 }
---
+++ src/test/java/ro/mycode/onlineclinicapi/repo/AppointmentRepoTest.java
@@ -0,0 +1,108 @@
+package ro.mycode.onlineclinicapi.repo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ro.mycode.onlineclinicapi.OnlineClinicApiApplication;
+import ro.mycode.onlineclinicapi.models.Appointment;
+import ro.mycode.onlineclinicapi.models.Doctor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = OnlineClinicApiApplication.class)
+class AppointmentRepoTest {
+
+    @Autowired
+    public AppointmentRepo appointmentRepo;
+
+    @BeforeEach
+    public void clean(){appointmentRepo.deleteAll();}
+
+    @Test
+    public void getAllAppointment(){
+        Appointment appointment = Appointment.builder().description("test").date(LocalDate.now()).number("07326482134").type("clinic").build();
+        Appointment appointment1 = Appointment.builder().description("test1").date(LocalDate.now()).number("07326482134").type("clinic").build();
+        Appointment appointment2 = Appointment.builder().description("test2").date(LocalDate.now()).number("07325324421").type("clinic").build();
+        Appointment appointment3 = Appointment.builder().description("test3").date(LocalDate.now()).number("07326542343").type("clinic").build();
+
+        appointmentRepo.save(appointment);
+        appointmentRepo.save(appointment1);
+        appointmentRepo.save(appointment2);
+        appointmentRepo.save(appointment3);
+
+        List<Appointment> appointmentList = new ArrayList<>();
+        appointmentList.add(appointment);
+        appointmentList.add(appointment1);
+        appointmentList.add(appointment2);
+        appointmentList.add(appointment3);
+
+        assertEquals(appointmentList,this.appointmentRepo.getAllAppointment());
+
+
+    }
+
+    @Test
+    public void getAppointmentByType(){
+
+
+        Appointment appointment = Appointment.builder().description("test").date(LocalDate.now()).number("07326482134").type("clinic").build();
+        Appointment appointment1 = Appointment.builder().description("test1").date(LocalDate.now()).number("07326482134").type("clinic").build();
+        Appointment appointment2 = Appointment.builder().description("test2").date(LocalDate.now()).number("07325324421").type("clinic").build();
+        Appointment appointment3 = Appointment.builder().description("test3").date(LocalDate.now()).number("07326542343").type("clinic").build();
+
+        appointmentRepo.save(appointment);
+        appointmentRepo.save(appointment1);
+        appointmentRepo.save(appointment2);
+        appointmentRepo.save(appointment3);
+
+        List<Appointment> appointmentList = new ArrayList<>();
+        appointmentList.add(appointment);
+        appointmentList.add(appointment1);
+        appointmentList.add(appointment2);
+        appointmentList.add(appointment3);
+
+        assertEquals(appointmentList,this.appointmentRepo.getAppointmentByType("clinic"));
+
+    }
+
+    @Test
+    public void getAppointmentByDate(){
+        Appointment appointment = Appointment.builder().description("test").date(LocalDate.of(2023,6,20)).number("07326482134").type("clinic").build();
+        Appointment appointment1 = Appointment.builder().description("test1").date(LocalDate.of(2023,6,20)).number("07326482134").type("clinic").build();
+        Appointment appointment2 = Appointment.builder().description("test2").date(LocalDate.of(2023,6,20)).number("07325324421").type("clinic").build();
+        Appointment appointment3 = Appointment.builder().description("test3").date(LocalDate.of(2023,6,20)).number("07326542343").type("clinic").build();
+
+        appointmentRepo.save(appointment);
+        appointmentRepo.save(appointment1);
+        appointmentRepo.save(appointment2);
+        appointmentRepo.save(appointment3);
+
+        List<Appointment> appointmentList = new ArrayList<>();
+        appointmentList.add(appointment);
+        appointmentList.add(appointment1);
+        appointmentList.add(appointment2);
+        appointmentList.add(appointment3);
+
+        assertEquals(appointmentList,this.appointmentRepo.getAppointmentByDate(LocalDate.of(2023,6,20)));
+    }
+
+    @Test
+    public void getAppointmentByNumber(){
+        Appointment appointment = Appointment.builder().description("test").date(LocalDate.of(2023,6,20)).number("07326482134").type("clinic").build();
+
+        appointmentRepo.save(appointment);
+
+
+
+        assertEquals(appointment,this.appointmentRepo.getAppointmentByNumber("07326482134").get());
+    }
+}
