da8415e4b45a3fc93f599db999434a330b8d8e45
Balanescu Raul <balanescuraul@gmail.com>
2023-06-02 23:38:13 +0300

    add patient-resource

---
+++ src/main/java/ro/mycode/onlineclinicapi/dto/CreateRestResponse.java
@@ -0,0 +1,13 @@
+package ro.mycode.onlineclinicapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateRestResponse {
+
+    public String message;
+}
---
+++ src/main/java/ro/mycode/onlineclinicapi/rest/PatientResource.java
@@ -0,0 +1,101 @@
+package ro.mycode.onlineclinicapi.rest;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ro.mycode.onlineclinicapi.dto.CreateRestResponse;
+import ro.mycode.onlineclinicapi.dto.PatientDTO;
+import ro.mycode.onlineclinicapi.models.Patient;
+import ro.mycode.onlineclinicapi.service.PatientService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping( value = "/api/v1/patient")
+@Slf4j
+public class PatientResource {
+
+    private PatientService patientService;
+
+    public PatientResource(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/getAllPatient")
+    public ResponseEntity<List<Patient>> getAllPatient(){
+    List<Patient> patientList = this.patientService.getAllPatient();
+
+    log.info("REST request to get all patient {}",patientList);
+
+    return new ResponseEntity<>(patientList, HttpStatus.OK);
+    }
+
+    @PostMapping("/addPatient")
+    public ResponseEntity<CreateRestResponse> addPatient(@RequestBody PatientDTO patientDTO){
+        log.info("REST request to add patient",patientDTO);
+
+        this.patientService.addPatient(patientDTO);
+
+        return new ResponseEntity<>(new CreateRestResponse("Patient was added!"),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/removePatient/patientName")
+    public ResponseEntity<CreateRestResponse> removePatient(@PathVariable String patientName){
+        log.info("REST request to delete patient by name",patientName);
+
+        this.patientService.deletePatient(patientName);
+
+        return new ResponseEntity<>(new CreateRestResponse("Patient was deleted!"),HttpStatus.OK);
+    }
+
+    @GetMapping("/getPatientbyId/id")
+    public ResponseEntity<Patient> getPatientById(@PathVariable int id){
+
+        log.info("REST request to get patient by id",id);
+
+       Patient patient =  this.patientService.getPatientbyId(id);
+
+        return new ResponseEntity<>(patient,HttpStatus.OK);
+
+    }
+
+    @GetMapping("/getPatientbyUsername/username")
+    public ResponseEntity<Patient> getPatientByUsername(@PathVariable String username){
+        log.info("REST request to get patient by username",username);
+
+        Patient patient = this.patientService.getPatientByUsername(username);
+
+        return new ResponseEntity<>(patient,HttpStatus.OK);
+    }
+
+    @GetMapping("/getPatientByEmail/email")
+    public ResponseEntity<Patient> getPatientByEmail(@PathVariable String email){
+        log.info("REST request to get patient by email",email);
+
+        Patient patient = this.patientService.getPatientByEmail(email);
+
+        return new ResponseEntity<>(patient,HttpStatus.OK);
+    }
+
+    @GetMapping("/getPatientByFullName")
+    public ResponseEntity<Patient> getPatientByFullName(@RequestParam String fullName){
+        log.info("REST request to get patient by full name",fullName);
+
+        Patient patient = this.patientService.getPatientbyFullName(fullName);
+
+        return new ResponseEntity<>(patient,HttpStatus.OK);
+    }
+
+    @PutMapping("/updatePatient")
+    public ResponseEntity<CreateRestResponse> updatePatient(@RequestBody PatientDTO patientDTO){
+        log.info("REST request to update patient ",patientDTO);
+
+        this.patientService.updatePatient(patientDTO);
+
+        return new ResponseEntity<>(new CreateRestResponse("Patient was updated"),HttpStatus.OK);
+    }
+
+
+}
