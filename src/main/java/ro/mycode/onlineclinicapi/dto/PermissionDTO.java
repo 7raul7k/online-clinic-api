99a4696f05e82bdd05918c4a1327054450293c09
Balanescu Raul <balanescuraul@gmail.com>
2023-06-03 22:29:29 +0300

    add permission-resource

--- src/main/java/ro/mycode/onlineclinicapi/dto/PermissionDTO.java
+++ src/main/java/ro/mycode/onlineclinicapi/dto/PermissionDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import ro.mycode.onlineclinicapi.models.Role;
 
 @Data
 @AllArgsConstructor
@@ -15,5 +16,7 @@ public class PermissionDTO {
     private String title;
     private String module;
     private String description;
+
+    private Role role;
 
 }
